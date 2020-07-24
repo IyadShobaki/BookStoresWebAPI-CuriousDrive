defa5411c7ea38cf3d3a42cac307135ece5eaa18
IyadShobaki <iyadshobaki82@gmail.com>
2020-07-24 16:22:27 -0400

    Add Data to Publisher Table
    Add hardcoded data using the API using visual studio and Postman.

--- BookStoresWebAPI/Controllers/PublishersController.cs
+++ BookStoresWebAPI/Controllers/PublishersController.cs
@@ -28,16 +28,75 @@ namespace BookStoresWebAPI.Controllers
         }
 
         // GET: api/Publishers/5
+        [HttpGet("PostPublisherDetails/")]
+        public async Task<ActionResult<Publisher>> PostPublisherDetails()
+        {
+
+            var publisher = new Publisher();
+            publisher.PublisherName = "Harper & Brothers";
+            publisher.City = "New York City";
+            publisher.State = "NY";
+            publisher.Country = "USA";
+
+            Book book1 = new Book();
+            book1.Title = "Good Night Moon - 1";
+            book1.PublishedDate = DateTime.Now;
+
+            Book book2 = new Book();
+            book2.Title = "Good Night Moon - 2";
+            book2.PublishedDate = DateTime.Now;
+
+            Sale sale1 = new Sale();
+            sale1.Quantity = 2;
+            sale1.StoreId = "8042";
+            sale1.OrderNum = "XYZ";
+            sale1.PayTerms = "Net 30";
+            sale1.OrderDate = DateTime.Now;
+
+            Sale sale2 = new Sale();
+            sale2.Quantity = 2;
+            sale2.StoreId = "7131";
+            sale2.OrderNum = "QA879.1";
+            sale2.PayTerms = "Net 20";
+            sale2.OrderDate = DateTime.Now;
+
+            book1.Sales.Add(sale1);
+            book2.Sales.Add(sale2);
+
+            publisher.Books.Add(book1);
+            publisher.Books.Add(book2);
+
+            _context.Publishers.Add(publisher);
+            _context.SaveChanges();
+
+            var publishers = await _context.Publishers
+                .Include(pub => pub.Books)
+                    .ThenInclude(book => book.Sales)
+                .Include(pub => pub.Users)
+                    .ThenInclude(user => user.Job)
+                .Where(pub => pub.PubId == publisher.PubId)
+                .FirstOrDefaultAsync();
+
+            if (publishers == null)
+            {
+                return NotFound();
+            }
+
+            return publishers;
+        }
+
+
+        // GET: api/Publishers/5
         [HttpGet("GetPublisherDetails/{id}")]
         public async Task<ActionResult<Publisher>> GetPublisherDetails(int id)
         {
-            var publisher = _context.Publishers
+            var publisher = await _context.Publishers
                 .Include(pub => pub.Books)
                     .ThenInclude(book => book.Sales)
                 .Include(pub => pub.Users)
                     .ThenInclude(user => user.Job)
                 .Where(pub => pub.PubId == id)
-                .FirstOrDefault();
+                .FirstOrDefaultAsync();
 
             if (publisher == null)
             {
