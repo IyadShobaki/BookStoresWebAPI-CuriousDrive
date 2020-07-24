57619cae9a19d53c1b11fa4c8b7b6c539b73fae5
IyadShobaki <iyadshobaki82@gmail.com>
2020-07-24 18:50:46 -0400

    Update the Database

--- BookStoresWebAPI/Models/BookStoresDBContext.cs
+++ BookStoresWebAPI/Models/BookStoresDBContext.cs
@@ -20,6 +20,8 @@ namespace BookStoresWebAPI.Models
         public virtual DbSet<BookAuthor> BookAuthors { get; set; }
         public virtual DbSet<Job> Jobs { get; set; }
         public virtual DbSet<Publisher> Publishers { get; set; }
+        public virtual DbSet<RefreshToken> RefreshTokens { get; set; }
+        public virtual DbSet<Role> Roles { get; set; }
         public virtual DbSet<Sale> Sales { get; set; }
         public virtual DbSet<Store> Stores { get; set; }
         public virtual DbSet<User> Users { get; set; }
@@ -209,6 +211,46 @@ namespace BookStoresWebAPI.Models
                     .IsFixedLength();
             });
 
+            modelBuilder.Entity<RefreshToken>(entity =>
+            {
+                entity.HasKey(e => e.TokenId);
+
+                entity.ToTable("RefreshToken");
+
+                entity.Property(e => e.TokenId).HasColumnName("token_id");
+
+                entity.Property(e => e.ExpiryDate)
+                    .HasColumnName("expiry_date")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.Token)
+                    .IsRequired()
+                    .HasColumnName("token")
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UserId).HasColumnName("user_id");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.RefreshTokens)
+                    .HasForeignKey(d => d.UserId)
+                    .HasConstraintName("FK__RefreshTo__user___60FC61CA");
+            });
+
+            modelBuilder.Entity<Role>(entity =>
+            {
+                entity.ToTable("Role");
+
+                entity.Property(e => e.RoleId).HasColumnName("role_id");
+
+                entity.Property(e => e.RoleDesc)
+                    .IsRequired()
+                    .HasColumnName("role_desc")
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasDefaultValueSql("('New Position - title not formalized yet')");
+            });
+
             modelBuilder.Entity<Sale>(entity =>
             {
                 entity.ToTable("Sale");
@@ -294,18 +336,20 @@ namespace BookStoresWebAPI.Models
             modelBuilder.Entity<User>(entity =>
             {
                 entity.HasKey(e => e.UserId)
-                    .HasName("PK_user_id")
+                    .HasName("PK_user_id_2")
                     .IsClustered(false);
 
                 entity.ToTable("User");
 
-                entity.Property(e => e.UserId)
-                    .HasColumnName("user_id")
-                    .HasMaxLength(50)
+                entity.Property(e => e.UserId).HasColumnName("user_id");
+
+                entity.Property(e => e.EmailAddress)
+                    .IsRequired()
+                    .HasColumnName("email_address")
+                    .HasMaxLength(100)
                     .IsUnicode(false);
 
                 entity.Property(e => e.FirstName)
-                    .IsRequired()
                     .HasColumnName("first_name")
                     .HasMaxLength(20)
                     .IsUnicode(false);
@@ -315,16 +359,7 @@ namespace BookStoresWebAPI.Models
                     .HasColumnType("datetime")
                     .HasDefaultValueSql("(getdate())");
 
-                entity.Property(e => e.JobId)
-                    .HasColumnName("job_id")
-                    .HasDefaultValueSql("((1))");
-
-                entity.Property(e => e.JobLevel)
-                    .HasColumnName("job_level")
-                    .HasDefaultValueSql("((10))");
-
                 entity.Property(e => e.LastName)
-                    .IsRequired()
                     .HasColumnName("last_name")
                     .HasMaxLength(30)
                     .IsUnicode(false);
@@ -336,22 +371,34 @@ namespace BookStoresWebAPI.Models
                     .IsFixedLength();
 
                 entity.Property(e => e.Password)
+                    .IsRequired()
                     .HasColumnName("password")
                     .HasMaxLength(100)
                     .IsUnicode(false);
 
-                entity.Property(e => e.PubId).HasColumnName("pub_id");
+                entity.Property(e => e.PubId)
+                    .HasColumnName("pub_id")
+                    .HasDefaultValueSql("((1))");
 
-                entity.HasOne(d => d.Job)
-                    .WithMany(p => p.Users)
-                    .HasForeignKey(d => d.JobId)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("FK__Users__job_id__114A936A");
+                entity.Property(e => e.RoleId)
+                    .HasColumnName("role_id")
+                    .HasDefaultValueSql("((1))");
+
+                entity.Property(e => e.Source)
+                    .IsRequired()
+                    .HasColumnName("source")
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
 
                 entity.HasOne(d => d.Pub)
                     .WithMany(p => p.Users)
                     .HasForeignKey(d => d.PubId)
-                    .HasConstraintName("FK__Users__pub_id__607251E5");
+                    .HasConstraintName("FK__User2__pub_id__60083D91");
+
+                entity.HasOne(d => d.Role)
+                    .WithMany(p => p.Users)
+                    .HasForeignKey(d => d.RoleId)
+                    .HasConstraintName("FK__User__role_id__6E565CE8");
             });
 
             OnModelCreatingPartial(modelBuilder);
